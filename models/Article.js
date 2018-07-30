b665f4851c0cb03fe6bdc3f72362da5340d19957
RRaethke <reece.raethke@outlook.com>
2018-07-30 13:06:45 -0500

    styling changes

--- models/Article.js
+++ models/Article.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 // save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// create a new article schema. The link should be unique, but the other properties are not required because they may not exist on the website to be scraped. There is validation on the route to add them to the database on if these properties exist.
+// create a new article schema.
 var ArticleSchema = new Schema({
   title: {
     type: String,
